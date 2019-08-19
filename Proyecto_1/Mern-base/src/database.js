406f3f6074d9b23d4b184ccfb75390b8d62b2930
Santiago ortiz <sortizc1@eafit.edu.co>
2019-08-18 23:12:52 -0500

    dart

--- Proyecto_1/Mern-base/src/database.js
+++ Proyecto_1/Mern-base/src/database.js
@@ -1,12 +1,12 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 
 //const URI ='mongodb://localhost/mern-post'
-const URI ='mongodb://mongo-server/ProyectoMERN';
+const URI = 'mongodb://mongo-server/ProyectMERN';
 
 //sudo fuser -k 3000/tcp para error 
 
 mongoose.connect(URI)
-    .then(db => console.log('DB is connected'))
-    .catch(err => console.err(err));
+  .then(db => console.log('DB conectada'))
+  .catch(error => console.error(error));
 
-module.export =mongoose;
+module.exports = mongoose;
